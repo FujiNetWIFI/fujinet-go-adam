b86a5642a67eb8f0058cd4558de8944a152fee67
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-06-29 19:11:28 -0500

    bump version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,8 +123,8 @@ android {
         applicationId = "online.fujinet.go.adam"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5 
-        versionName = "0.10.0"
+        versionCode = 6 
+        versionName = "0.10.1"
         buildConfigField("String", "ADAMEM_VERSION", "\"${adamemVersion}\"")
         buildConfigField("String", "FUJINET_RUNTIME_VERSION", "\"${fujiNetRuntimeVersion}\"")
 
